b54def44d900d9e25f9f5744963a64c2bf5b2cbd
Aahish Abbani <shibe.aahish@gmail.com>
2026-07-10 16:21:21 -0400

    Initial Commit

---
+++ ringmvp/ringmvpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ringmvpApp.swift
+//  ringmvp
+//
+//  Created by Aahish Abbani on 7/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ringmvpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
